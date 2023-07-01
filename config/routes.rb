0d09b3f7fd15570ac47aef1c4daa007b6215aa8a
alvinkenyagah <alvin47k@gmail.com>
2023-07-01 21:01:08 +0300

    Completed assignment$

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only:[:show, :create]
+  # resources :users, only:[:show, :create]
   post '/signup', to: 'users#create'
   post "/login", to: "sessions#create"
   get "/me", to: "users#show"
